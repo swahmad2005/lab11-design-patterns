73e13410eb5a55a3bcc32955e96365c01c70818a
Saad Ahmad <150076462+swahmad2005@users.noreply.github.com>
2025-11-19 14:41:29 +0000

    Included final tests to ensure first alphanumeric character returned

--- presidio-anonymizer/tests/operators/test_initial.py
+++ presidio-anonymizer/tests/operators/test_initial.py
@@ -10,7 +10,10 @@ def test_correct_name():
     "input_text, initials",
     [
         ("John Smith", "J. S."),
-        ("     Eastern    Michigan   University ", "E. M. U.")
+        ("     Eastern    Michigan   University ", "E. M. U."),
+        ("@abc", "@A."),
+        ("@843A", "@8."),
+        ("--**abc", "--**A.")
     ],
 )
 def test_given_value_for_initial(input_text, initials):
