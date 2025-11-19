12c690edcf6f5c8c0ce51516982f87b23d5f46e1
Saad Ahmad <150076462+swahmad2005@users.noreply.github.com>
2025-11-19 14:01:28 +0000

    Included tests to verify functionality of initial operator

---
+++ presidio-anonymizer/tests/operators/test_initial.py
@@ -0,0 +1,17 @@
+import pytest
+
+from presidio_anonymizer.operators import Initial
+from presidio_anonymizer.entities import InvalidParamError
+
+def test_correct_name():
+    assert Initial().operator_name() == "initial"
+
+@pytest.mark.parametrize(
+    "input_text, initials",
+    [
+        ("John Smith", "J. S."),
+    ],
+)
+def test_given_value_for_initial(input_text, initials):
+    text = Initial().operate(input_text) == initials
+    assert text == initials
